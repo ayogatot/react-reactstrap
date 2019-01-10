aa702ff16e057997a3c580831a3325e4b3e5afab
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-10 23:44:46 +0700

    Add some picture and add carousel section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-import  Header  from "./components/Header";
+import Header  from "./components/Header";
+import CarouselSection from "./components/Carousel";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <CarouselSection />
       </div>
     );
   }
